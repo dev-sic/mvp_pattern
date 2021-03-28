830f8f6b4fc8327db52f074be063b66e63ce38de
dev_sic <tjdtlrl92@naver.com>
2021-03-28 23:53:01 +0900

    v1.0

--- app/src/main/java/com/example/mvp_pattern/MainActivity.kt
+++ app/src/main/java/com/example/mvp_pattern/MainActivity.kt
@@ -7,5 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //반영이 안된다.
     }
 }
